0159858dd6b713a3cec6df73f3808f02efc9bcac
Dichiara25 <edclrcq@gmail.com>
2023-01-08 18:37:57 +0100

    Fixed layout

--- src/layout/Home.tsx
+++ src/layout/Home.tsx
@@ -12,26 +12,38 @@ export function Home() {
     return (
         <div className={styles.container}>
             <main className={styles.main}>
-                <div className={styles.flex}>
-                    <button
-                        className={selected === "deposit" ? styles.selected : styles.unselected}
-                        onClick={() => setSelected("deposit")}
-                    >
-                        Deposit
-                    </button>
-                    <button
-                        className={selected === "withdraw" ? styles.selected : styles.unselected}
-                        onClick={() => setSelected("withdraw")}
-                    >
-                        Withdraw
-                    </button>
+                <div className={styles.center}>
+                    <div className={styles.flex}>
+                        <div className={styles.column}>
+                            <div className={styles.card}>
+                                <p/>
+                                <div className={styles.center}>
+                                    <div className={styles.flex}>
+                                        <button
+                                            className={selected === "deposit" ? styles.selected : styles.unselected}
+                                            onClick={() => setSelected("deposit")}
+                                        >
+                                            Deposit
+                                        </button>
+                                        <button
+                                            className={selected === "withdraw" ? styles.selected : styles.unselected}
+                                            onClick={() => setSelected("withdraw")}
+                                        >
+                                            Withdraw
+                                        </button>
+                                    </div>
+                                </div>
+                                <p/>
+                            </div>
+                            {selected === "deposit" ?
+                                <Deposit pool={pool} setPool={setPool} mixingFees={mixingFees}/>
+                                :
+                                <Withdraw pool={pool} setPool={setPool}/>
+                            }
+                        </div>
+                        <History/>
+                    </div>
                 </div>
-                {selected === "deposit" ?
-                    <Deposit pool={pool} setPool={setPool} mixingFees={mixingFees}/>
-                    :
-                    <Withdraw pool={pool} setPool={setPool}/>
-                }
-                <History/>
             </main>
         </div>
     );
